17f4c866fe25cceb699e30b26bf77115dd735138
emir derouiche <memir0@hotmail.no>
2020-03-28 15:54:50 +0100

    removed prod build from ignore

---
+++ build/precache-manifest.cc3f3185aa5dcbbdaf0db5df9aa4eceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c826e43a657bd2a18c973c9cd129821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da9a42636d8970d64d9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb609a22f9a9204dd468",
+    "url": "/static/js/2.9ef0d255.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.9ef0d255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da9a42636d8970d64d9",
+    "url": "/static/js/main.f2b1e2e5.chunk.js"
+  },
+  {
+    "revision": "3b18f8713049121d2e53",
+    "url": "/static/js/runtime-main.c0c75ad3.js"
+  }
+]);
