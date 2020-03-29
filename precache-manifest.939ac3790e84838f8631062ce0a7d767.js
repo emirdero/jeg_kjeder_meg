dc45ea887e439b6c34f34302b16fb6e819e836fc
emir derouiche <memir0@hotmail.no>
2020-03-29 01:29:00 +0100

    Updates

---
+++ precache-manifest.939ac3790e84838f8631062ce0a7d767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4989d391e0983a9daeab86c7f96cdab",
+    "url": "/jeg_kjeder_meg/index.html"
+  },
+  {
+    "revision": "ef56cdeb68850bf749c1",
+    "url": "/jeg_kjeder_meg/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb609a22f9a9204dd468",
+    "url": "/jeg_kjeder_meg/static/js/2.9ef0d255.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/jeg_kjeder_meg/static/js/2.9ef0d255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef56cdeb68850bf749c1",
+    "url": "/jeg_kjeder_meg/static/js/main.7b50cd86.chunk.js"
+  },
+  {
+    "revision": "3bbce1feabb268d1f415",
+    "url": "/jeg_kjeder_meg/static/js/runtime-main.a9d9b534.js"
+  }
+]);
